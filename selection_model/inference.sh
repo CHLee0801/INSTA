792d26cba898bd4f5a74d140d7a9c04f5d07c47e
changho.lee <changho.lee@lgresearch.ai>
2024-10-07 15:32:35 +0900

    delete file

--- selection_model/inference.sh
+++
@@ -1,9 +0,0 @@
-CUDA_VISIBLE_DEVICES=5 python inference_max_of_max.py more_filter_1130/173 > more_1130_epoch0.txt &
-sleep 2
-CUDA_VISIBLE_DEVICES=1 python inference_max_of_max.py more_filter_1130/346 > more_1130_epoch11.txt &
-sleep 2
-CUDA_VISIBLE_DEVICES=2 python inference_max_of_max.py more_filter_1130/519 > more_1130_epoch2.txt &
-sleep 2
-CUDA_VISIBLE_DEVICES=3 python inference_max_of_max.py more_filter_1130/692 > more_1130_epoch3.txt &
-sleep 2
-CUDA_VISIBLE_DEVICES=4 python inference_max_of_max.py more_filter_1130/865 > more_1130_epoch4.txt 
